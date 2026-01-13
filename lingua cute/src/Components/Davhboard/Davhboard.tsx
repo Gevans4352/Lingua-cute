842d464ec3ada9baae15c784b7ff383f249671fc
Gevans4352 <emogoghena@gmail.com>
2026-01-13 15:03:40 +0100

    dashboard sidebar

--- lingua cute/src/Components/Davhboard/Davhboard.tsx
+++ lingua cute/src/Components/Davhboard/Davhboard.tsx
@@ -1,10 +1,18 @@
 import "./Davhboard.scss";
-
+import { MdOutlineSpaceDashboard } from "react-icons/md";
+import { FaRegBell } from "react-icons/fa";
 const Davhboard = () =>{
     return(
-        <>
-        Hello from react 
-        </>
+        <section className="navboard">
+            <button><span><MdOutlineSpaceDashboard /></span></button>
+            <p>Dashboard</p>
+           <nav className="davion">
+            <ul>
+               <li><FaRegBell /></li>
+                <li>user</li>
+            </ul>
+           </nav>
+        </section>
     )
 }
 
--- lingua cute/src/Components/LeftBar/LeftBar.tsx
+++ lingua cute/src/Components/LeftBar/LeftBar.tsx
@@ -1,10 +1,30 @@
-import "./LeftBar";
-
+import "./LeftBar.scss"
+import { IoHomeOutline } from "react-icons/io5";
+import { LuBookOpen } from "react-icons/lu";
+import { MdOutlineTranslate } from "react-icons/md";
+import { IoGameControllerOutline } from "react-icons/io5";
+import { IoTrophyOutline } from "react-icons/io5";
+import { FiUser } from "react-icons/fi";
+import { RiSettings5Line } from "react-icons/ri";
+import { IoLogOutOutline } from "react-icons/io5";
 const LeftBar = () =>{
     return(
-        <>
-        Left bar 
-        </>
+        <section className="leftBar">
+            <p>Main Menu</p>
+            <div className="menu">
+                <button><span><IoHomeOutline /></span> Home</button>
+                <button><span><LuBookOpen /></span> Learning Progress</button>
+                <button><span><MdOutlineTranslate /></span> My Translation</button>
+                <button><span><IoGameControllerOutline /></span> Games & Challenge</button>
+                <button><span><IoTrophyOutline /></span> LeaderBoard</button>
+            </div>
+            <p>Account</p>
+            <div className="accounts">
+                <button><span><FiUser /></span> Profile</button>
+                <button><span><RiSettings5Line /></span> Settings</button>
+            </div>
+            <button><span><IoLogOutOutline /></span> Logout</button>
+        </section>
     )
 }
 
