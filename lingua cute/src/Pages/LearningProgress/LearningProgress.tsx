d82b453a81f5b1f858f4bd92bc101cf83891e93d
Gevans4352 <emogoghena@gmail.com>
2026-02-03 08:09:35 +0100

    changed

--- lingua cute/src/Pages/LearningProgress/LearningProgress.tsx
+++ lingua cute/src/Pages/LearningProgress/LearningProgress.tsx
@@ -7,14 +7,14 @@ import XPtab from "../../Components/XPtab/XPtab"
 const LearningProgress = ()=>{
     const [open, setOpen] = useState(false);
     useEffect(() => {
-        document.title = "Your Progress";
+        document.title = "Your Progrjjss";
     }, []);
     return(
        <section className="redo">
     <LeftBar isOpen={open} onClose={() => setOpen(false)} />
     <div className="main-right-side">
         <Davhboard onToggleSidebar={() => setOpen(!open)} />
-        <div className="actual-page-content">
+        <div className="actual-pajjj-content">
            <XPtab/>
         </div>
     </div>
