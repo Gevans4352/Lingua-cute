783dd891f4ee68f0a08f92104f065956eb2741dc
Gevans4352 <emogoghena@gmail.com>
2026-06-02 21:52:41 +0100

    fix: update server configuration

--- lingua cute/backend/src/server.js
+++ lingua cute/backend/src/server.js
@@ -1,8 +1,9 @@
+dotenv.config();
 import app from "./app.js";
 import dotenv from "dotenv";
 import connectDB from "./db.js";
 
-dotenv.config();
+
 
 const PORT = process.env.PORT || 5000;
 
@@ -14,10 +15,8 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`🚀 Backend running on http://localhost:${PORT}`);
+  console.log(` Backend running on http://localhost:${PORT}`);
 });
 
 
-// yPYN9G50JyYsDmEC
-//darasimifemiolagundoye_db_user
-//npm install mongodb
+
