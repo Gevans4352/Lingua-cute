211f017fccaa154c452b3e6baeac1a9efcc4936d
Gevans4352 <emogoghena@gmail.com>
2026-06-02 21:57:56 +0100

    fix: update register page

--- lingua cute/src/Pages/Register/Register.tsx
+++ lingua cute/src/Pages/Register/Register.tsx
@@ -1,6 +1,7 @@
 import { Link, useNavigate } from "react-router-dom";
 import "./Register.scss";
-import { useEffect, useState } from "react";
+import { useDocumentTitle } from "../../Hooks/useDocumentTitle";
+import { useState } from "react";
 import { MdOutlineMail } from "react-icons/md";
 import { TbLockPassword } from "react-icons/tb";
 import { CiUser } from "react-icons/ci";
@@ -8,73 +9,97 @@ import googleIcon from "../../assets/google-icon-logo-svgrepo-com.svg";
 import { IoEyeOutline, IoEyeOffOutline } from "react-icons/io5";
 
 const Register = () => {
+  type Errors = {
+    email?: string;
+    password?: string;
+    name?: string;
+  };
   const navigate = useNavigate();
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState<Errors>({});
   const [showPassword, setShowPassword] = useState(false);
-  useEffect(() => {
-    document.title = "Register";
-  }, []);
+  useDocumentTitle("Register");
   const handleRegister = async (e: React.FormEvent) => {
     e.preventDefault();
-    if (email.trim().length === 0) {
-      alert("Email address is required.");
-      return;
-    }
-    if (!email.includes("@")) {
-      alert("Please enter a valid email address");
-      return;
-    }
-    if (
-      email.indexOf("@") === -1 ||
-      email.indexOf(".") === -1 ||
-      email.indexOf(".") < email.indexOf("@")
-    ) {
-      alert("Please enter a valid email address");
-      return;
-    }
-    if (password.length < 8) {
-      alert("Password must be at least 8 characters long.");
-      return;
-    }
-    if (!/\d/.test(password)) {
-      alert("Password must contain at least one number.");
-      return;
-    }
-    if (!/[A-Z]/.test(password)) {
-      alert("Password must contain at least one uppercase letter.");
-      return;
-    }
-    if (!/^[a-zA-Z0-9]+$/.test(name)) {
-      alert("Username must only contain letters and numbers.");
-      return;
-    }
-    if (setName.length > 20) {
-      alert("Username cannot exceed 20 characters.");
-      return;
+    try {
+      const res = await fetch("http://localhost:5000/api/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password, name }),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (res.ok) {
+        //SAVE TOKEN & USER
+        localStorage.setItem("token", data.token);
+        localStorage.setItem("user", JSON.stringify(data.user));
+        navigate("/login");
+      } else {
+        setErrors({ email: data.message });
+      }
+    } catch (error) {
+      console.error(error);
     }
 
-   try {
-    const res = await fetch("http://localhost:5000/api/auth/register", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ email, password, name }),
-    });
-    const data = await res.json();
-    if (!res.ok) {
-      alert(data.message || "Login failed");
-      return;
+    const newErrors: Errors = {};
+
+    if (email.trim().length === 0) {
+      newErrors.email = "Email address is required.";
     }
-    // save user
-    localStorage.setItem("user", JSON.stringify(data.user));
+    if (!email.includes("@") || !email.includes(".")) {
+      newErrors.email = "Please enter a valid email address";
+    }
+    if (password.length < 8) {
+      newErrors.password = "Password must be at least 8 characters long.";
+    }
+    let hasNumber = false;
+    for (let i = 0; i < password.length; i++) {
+      if (password[i] >= "0" && password[i] <= "9") {
+        hasNumber = true;
+        break;
+      }
+    }
+    if (!hasNumber) {
+      newErrors.password = "Password must contain at least one number.";
+    }
+    let hasLetter = false;
+    for (let i = 0; i < password.length; i++) {
+      if (
+        (password[i] >= "a" && password[i] <= "z") ||
+        (password[i] >= "A" && password[i] <= "Z")
+      ) {
+        hasLetter = true;
+        break;
+      }
+    }
+    if (!hasLetter) {
+      newErrors.password =
+        "Password must contain at least one uppercase letter.";
+    }
+    let isValid = true;
+    for (let i = 0; i < name.length; i++) {
+      const char = name[i];
+      const isLetter =
+        (char >= "a" && char <= "z") || (char >= "A" && char <= "Z");
+      const isNumber = char >= "0" && char <= "9";
+      if (!isLetter && !isNumber) {
+        isValid = false;
+        break;
+      }
+    }
+    if (!isValid) {
+      newErrors.name = "Username must only contain letters and numbers.";
+    }
+    if (setName.length > 20) {
+      newErrors.name = "Username cannot exceed 20 characters.";
+    }
+    setErrors(newErrors);
+    if (Object.keys(newErrors).length > 0) return;
     navigate("/Login");
-  } catch (err) {
-    alert("Server problem");
-  }
-    alert("Network error. Make sure backend is running on port 5000");
   };
 
   return (
@@ -88,12 +113,13 @@ const Register = () => {
               <CiUser />
               <input
                 type="text"
-                placeholder="Your name"
+                placeholder="Enter your name"
                 value={name}
                 onChange={(e) => setName(e.target.value)}
                 name="name"
                 required
               />
+              {errors.name && <p className="error-text">{errors.name}</p>}
             </div>
             <div className="input-wrapper">
               <MdOutlineMail />
@@ -105,6 +131,7 @@ const Register = () => {
                 name="email"
                 required
               />
+              {errors.email && <p className="error-text">{errors.email}</p>}
             </div>
             <div className="input-wrapper">
               <TbLockPassword />
@@ -112,6 +139,7 @@ const Register = () => {
                 type={showPassword ? "text" : "password"}
                 placeholder="Enter your password"
                 value={password}
+                name="password"
                 onChange={(e) => setPassword(e.target.value)}
               />
               <button
@@ -123,6 +151,7 @@ const Register = () => {
                 {showPassword ? <IoEyeOutline /> : <IoEyeOffOutline />}
               </button>
             </div>
+            {errors.password && <p className="error-text">{errors.password}</p>}
             <button type="submit">Sign In</button>
           </form>
           <div className="divider">
